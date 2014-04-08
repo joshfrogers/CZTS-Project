56134d3b2b88b00815153bd2e3f89e0d6eb2b5b8
Joshua Rogers <freeminddot@gmail.com>
2014-04-08 10:29:13 +0100

    08/04/14 Update - Almost finished, Adding bits from Scragg/Moh

--- Python/3d/3dTetrahedron.py
+++ Python/3d/3dTetrahedron.py
@@ -2,8 +2,10 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig=plt.figure()
-ax = Axes3D(fig)
+fig=plt.figure(figsize=plt.figaspect(0.75)*1.5)#num=None, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
+ax = fig.gca(projection='3d')#Axes3D(fig)
+
+
 
 # Face 1
 x1 = np.array([[0, 0.5, 0.5, 1],
@@ -36,11 +38,12 @@ y4 = np.array([[0, 0, 0.433013, 0],
 z4 = np.array([[0, 0, 0.866, 0],
                [0, 0, 0, 0]])
 
-#ax.plot_surface(x1,y1,z1)
-ax.plot_surface(x2,y2,z2)
-ax.plot_surface(x3,y3,z3)
-ax.plot_surface(x4,y4,z4)
-print x1
+ax.plot_wireframe(x1,y1,z1)
+ax.plot_wireframe(x2,y2,z2)
+ax.plot_wireframe(x3,y3,z3)
+ax.plot_wireframe(x4,y4,z4)
 
-#0.433
+ax.set_autoscalez_on(True)
+ax.set_autoscale_on(True)
+
 plt.show()
